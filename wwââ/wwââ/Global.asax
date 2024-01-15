1c73ec45596a13eaa000b371b9e183c440265ea2
luan <nguyenthanhluan230703@gmail.com>
2024-01-15 23:56:28 +0700

    final commit

---
+++ wwââ/wwââ/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wwââ.MvcApplication" Language="C#" %>
---
+++ wwââ/wwââ/Scripts/JavaScript.js
@@ -0,0 +1,31 @@
+﻿<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+        $(".subcategory-link").click(function (e) {
+            e.preventDefault();
+
+            var categoryId = $(this).data("category-id");
+
+            $.ajax({
+                url: '/ControllerName/GetProductsByCategory',
+                type: 'GET',
+                data: { categoryId: categoryId },
+                dataType: 'json',
+                success: function (data) {
+                    // Assuming you have a container with the id "product-container" to display the products
+                    $("#product-container").empty();
+
+                    // Iterate through the returned data and update the product container
+                    $.each(data, function (index, product) {
+                        // Customize this part based on your product data structure
+                        $("#product-container").append("<div>" + product.ProductName + "</div>");
+                    });
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        });
+    });
+</script>
